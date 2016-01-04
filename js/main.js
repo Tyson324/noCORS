e1977b5e0a32be22cf27311ede3346e75f8e554d
Sibelious <sibelious@outlook.com>
2016-01-04 16:35:25 +0000

    dsa

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 		
 	$("#makeRequest").click(function(){
 		alert("ow");
-$.getJSON('http://anyorigin.com/get?url=http%3A//tympanus.net/codrops/&callback=?', function(data){
+$.getJSON('http://anyorigin.com/get?url=https%3A//www.explore.ms/navigation.aspx&callback=?', function(data){
 	$('#output').html(data.contents);
 });
 		//make request
